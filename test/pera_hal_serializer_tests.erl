c3a5501de8f9aa244c97af8590c3ecbd02db3f37
Farruco Sanjurjo <madtrick@gmail.com>
2014-02-08 14:50:08 +0100

    Modify the hal_serializer tests to use the macros

--- test/pera_hal_serializer_tests.erl
+++ test/pera_hal_serializer_tests.erl
@@ -3,20 +3,20 @@
 -include("pera_hal_macros.hrl").
 
 empty_resource_to_json_test() ->
-  JSON = pera_hal_serializer:to_json(pera_hal:resource([], [], [])),
+  JSON = pera_hal_serializer:to_json(?HAL_RESOURCE([], [], [])),
 
   ?assert(JSON == <<"{\"_links\":{},\"_embedded\":{}}">>).
 
 resource_with_properties_to_json_test() ->
   JSON = pera_hal_serializer:to_json(
-    pera_hal:resource([], [], [?HAL_PROPERTY_OBJECT([{1, 2}])])
+    ?HAL_RESOURCE([], [], [?HAL_PROPERTY_OBJECT([{1, 2}])])
   ),
 
   ?assert(JSON == <<"{\"_links\":{},\"_embedded\":{},\"1\":2}">>).
 
 resource_with_nested_properties_to_json_test() ->
   JSON = pera_hal_serializer:to_json(
-    pera_hal:resource(
+    ?HAL_RESOURCE(
       [],
       [],
       [
@@ -34,7 +34,7 @@ resource_with_nested_properties_to_json_test() ->
 
 resource_with_list_in_property_to_json_test() ->
   JSON = pera_hal_serializer:to_json(
-    pera_hal:resource(
+    ?HAL_RESOURCE(
       [],
       [],
       [?HAL_PROPERTY_OBJECT([ {1, [2, 3, 4]} ])]
@@ -46,7 +46,7 @@ resource_with_list_in_property_to_json_test() ->
 
 resource_with_nested_collection_of_objects_to_json_test() ->
   JSON = pera_hal_serializer:to_json(
-    pera_hal:resource(
+    ?HAL_RESOURCE(
       [],
       [],
       [
@@ -62,41 +62,42 @@ resource_with_nested_collection_of_objects_to_json_test() ->
   ?debugMsg(JSON),
   ?assert(JSON == <<"{\"_links\":{},\"_embedded\":{},\"1\":[{\"2\":3},{\"4\":5}]}">>).
 
-%resource_with_links_to_json_test() ->
-%  JSON = pera_hal_serializer:to_json(
-%    pera_hal:resource(
-%      [pera_hal:link(<<"self">>, <<"/self">>, [])],
-%      [],
-%      []
-%    )
-%  ),
+resource_with_links_to_json_test() ->
+  JSON = pera_hal_serializer:to_json(
+    ?HAL_RESOURCE(
+      [?HAL_LINK(self, <<"/self">>, [])],
+      [],
+      []
+    )
+  ),
 
-%  ?assert(JSON == <<"{\"_links\":{\"self\":{\"href\":\"/self\"}},\"_embedded\":{}}">>).
+  ?assert(JSON == <<"{\"_links\":{\"self\":{\"href\":\"/self\"}},\"_embedded\":{}}">>).
 
-%resource_with_links_and_embedded_to_json_test() ->
-%  JSON = pera_hal_serializer:to_json(
-%    pera_hal:resource(
-%      [pera_hal:link(<<"self">>, <<"/self">>, [])],
-%      [{<<"thing">>, pera_hal:resource([pera_hal:link(<<"self">>, <<"/self/1">>, [])], [], [])}],
-%      []
-%    )
-%  ),
+resource_with_links_and_embedded_to_json_test() ->
+  JSON = pera_hal_serializer:to_json(
+    ?HAL_RESOURCE(
+      [?HAL_LINK(self, <<"/self">>, [])],
+      [?HAL_EMBEDDED(<<"thing">>, ?HAL_RESOURCE([?HAL_LINK(self, <<"/self/1">>, [])], [], []))],
+      []
+    )
+  ),
 
 
-%  ?assert(JSON == <<"{\"_links\":{\"self\":{\"href\":\"/self\"}},\"_embedded\":{\"thing\":{\"_links\":{\"self\":{\"href\":\"/self/1\"}},\"_embedded\":{}}}}">>).
+  ?assert(JSON == <<"{\"_links\":{\"self\":{\"href\":\"/self\"}},\"_embedded\":{\"thing\":{\"_links\":{\"self\":{\"href\":\"/self/1\"}},\"_embedded\":{}}}}">>).
 
-%resource_with_links_and_embeddeds_to_json_test() ->
-%  JSON = pera_hal_serializer:to_json(
-%    pera_hal:resource(
-%      [pera_hal:link(<<"self">>, <<"/self">>, [])],
-%      [
-%        {<<"things">>, [
-%            pera_hal:resource([pera_hal:link(<<"self">>, <<"/self/1">>, [])], [], []),
-%            pera_hal:resource([pera_hal:link(<<"self">>, <<"/self/2">>, [])], [], [])
-%          ]}
-%      ],
-%      []
-%    )
-%  ),
+resource_with_links_and_embeddeds_to_json_test() ->
+  JSON = pera_hal_serializer:to_json(
+    ?HAL_RESOURCE(
+      [?HAL_LINK(self, <<"/self">>, [])],
+      [?HAL_EMBEDDED(<<"things">>,
+          [
+            ?HAL_RESOURCE([?HAL_LINK(self, <<"/self/1">>, [])], [], []),
+            ?HAL_RESOURCE([?HAL_LINK(self, <<"/self/2">>, [])], [], [])
+          ]
+        )
+      ],
+      []
+    )
+  ),
 
-%  ?assert(JSON == <<"{\"_links\":{\"self\":{\"href\":\"/self\"}},\"_embedded\":{\"things\":[{\"_links\":{\"self\":{\"href\":\"/self/1\"}},\"_embedded\":{}},{\"_links\":{\"self\":{\"href\":\"/self/2\"}},\"_embedded\":{}}]}}">>).
+  ?assert(JSON == <<"{\"_links\":{\"self\":{\"href\":\"/self\"}},\"_embedded\":{\"things\":[{\"_links\":{\"self\":{\"href\":\"/self/1\"}},\"_embedded\":{}},{\"_links\":{\"self\":{\"href\":\"/self/2\"}},\"_embedded\":{}}]}}">>).
